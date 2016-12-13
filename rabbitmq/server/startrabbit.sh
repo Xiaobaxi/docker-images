20082c7759578963f18aea2fdb2094bf4ef4d0e6
fangzhibin <silencon@gmail.com>
2016-12-13 22:49:14 +0800

    update the sh file

--- rabbitmq/server/startrabbit.sh
+++ rabbitmq/server/startrabbit.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 logfile="/tmp/rabbitnode.log"
-firsttimefile="/tmp/firsttimerunning"
 
 curhostname=`hostname`
 echo "" > $logfile
@@ -15,45 +14,6 @@ echo "Running: rabbitmqctl set_permissions -p $curhostname guest '.*' '.*' '.*'"
 /usr/sbin/rabbitmqctl set_permissions -p $curhostname guest ".*" ".*" ".*"  >> $logfile
 sleep 5
     
-# For version 3.5.6 the first time running the cluster needs to enable the plugins
-if [ -f $firsttimefile ]; then
-  echo "First Time Running Enabling Plugins" >> $logfile
-  /usr/sbin/rabbitmq-server -d &
-  echo "Waiting for RabbitMQ Server to start" >> $logfile
-  sleep 3
-  echo "Enabling Plugins" >> $logfile
-  /usr/sbin/rabbitmq-plugins enable rabbitmq_mqtt rabbitmq_stomp rabbitmq_management  rabbitmq_management_agent rabbitmq_management_visualiser rabbitmq_federation rabbitmq_federation_management sockjs >> $logfile
-  echo "Waiting for Plugins to finish" >> $logfile
-  sleep 1
-  echo "Stopping the RabbitMQ using stop_app" >> $logfile
-  /usr/sbin/rabbitmqctl stop_app
-  echo "Stopping the RabbitMQ using stop" >> $logfile
-  /usr/sbin/rabbitmqctl stop
-
-  echo "Stopping the RabbitMQ Server" >> $logfile
-  kill -9 `ps auwwx | grep rabbitmq-server | awk '{print $2}'`
-  sleep 1
-
-  echo "Done First Time Running Enabling Plugins" >> $logfile
-  rm -f $firsttimefile >> $logfile
-  echo "Done Cleanup First Time File" >> $logfile
-
-  
-  # Allow the cluster nodes to wait for the master to start the first time
-  if [ -z "$CLUSTERED" ]; then
-    echo "Ignoring as this is the server node" >> $logfile
-  else
-    if [ -z "$CLUSTER_WITH" ]; then
-      echo "Ignoring as this is the cluster master node" >> $logfile
-    else
-      echo "Waiting for the master node to start up" >> $logfile
-      sleep 5
-      echo "Done waiting for the master node to start up" >> $logfile
-    fi
-  fi
-fi
-
-
 if [ -z "$CLUSTERED" ]; then
 
   echo "Starting non-Clustered Server Instance" >> $logfile
